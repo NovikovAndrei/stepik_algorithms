1df43faccb56645e125287ef54e04e53cec37c47
Andrei_Novikov <novikovas1@mail.ru>
2023-03-19 23:36:53 +0300

    add

---
+++ Графы и деревья/Практические задания/Обход в ширину/task.py
@@ -0,0 +1,55 @@
+from typing import Hashable, List
+from collections import deque
+
+import networkx as nx
+
+
+def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
+    """
+    Функция выполняет обход в ширину и возвращает список узлов в порядке посещения.
+    В данной задаче порядок обхода графа левосторонний или правосторонний не важен,
+    главное соблюсти порядок обхода в ширину.
+
+    :param g: Граф NetworkX, по которому нужно совершить обход
+    :param start_node: Стартовый узел, откуда нужно начать обход
+    :return: Список узлов в порядке посещения.
+    """
+    visited = {node: False for node in g.nodes}
+    d = deque()
+    path = []
+
+    d.append(start_node)
+    visited[start_node] = True
+    while d:
+        current_node = d.popleft()
+        path.append(current_node)
+        for neighbor in g.neighbors(current_node):
+            if not visited[neighbor]:
+                d.append(neighbor)
+                visited[neighbor] = True
+    return path
+
+      # TODO реализовать обход в ширину
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFGHIJ")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'F'),
+        ('B', 'G'),
+        ('F', 'G'),
+        ('G', 'C'),
+        ('G', 'H'),
+        ('G', 'I'),
+        ('H', 'C'),
+        ('H', 'I'),
+        ('H', 'J'),
+        ('H', 'E'),
+        ('H', 'D'),
+        ('E', 'D'),
+    ])
+    # print(list(graph.neighbors("D")))
+
+    print(bfs(graph, "A"))
+
